519561910d72c55c14a7666c9be23acc2c2dde7d
ZoRb96 <sauravbhagabati@gmail.com>
2021-07-20 00:51:10 +0530

    Initial commit

---
+++ src/main/java/com/ABRAMS/ABRAMS/AbramsApplication.java
@@ -0,0 +1,13 @@
+package com.ABRAMS.ABRAMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbramsApplication.class, args);
+	}
+
+}
